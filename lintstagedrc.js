7af399a4ee2f13edc9f34d8a28105be396ec23e2
Wellvergton Marques <wmc.jp@outlook.com>
2024-05-27 20:37:16 -0300

    aula 14

---
+++ lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}``npm run lint --fix . ${filenames.join('--file')}`
+  ]
+};
