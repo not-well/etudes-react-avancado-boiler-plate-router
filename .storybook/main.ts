7d7af76e37c9f9b7c2758abc41d0184c92f15af9
Wellvergton Marques <wmc.jp@outlook.com>
2024-06-02 17:52:24 -0300

    aula 22 - storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  staticDirs: ['../public'],
+  webpackFinal: (config) => {
+    config.resolve?.modules?.push(`${process.cwd}/src`);
+
+    return config;
+  }
+};
+export default config;
